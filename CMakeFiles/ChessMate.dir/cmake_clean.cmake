3a06cafafdc45729298c4ead7fe2e04f2d232b88
Ivan Biryukov <vanyabiryukov@yandex.ru>
2021-11-28 19:16:54 +0300

    nothing

---
+++ CMakeFiles/ChessMate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessMate.dir/project/include/main.cpp.o"
+  "ChessMate"
+  "ChessMate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessMate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
