cf4db21195f2e2cd49c7b37a941b573bf7dccfd4
Andrew Razin <razin.andrew.ru@mail.ru>
2021-12-17 14:40:24 +0300

    reversed chang

---
+++ project/data/ClassMainDeck.hpp
@@ -0,0 +1,78 @@
+#ifndef PROJECT_CLASSMAINDECK_UTILS_HPP_
+#define PROJECT_CLASSMAINDECK_UTILS_HPP_
+
+#include "ClassDeck.hpp"
+#include "ClassCard.hpp"
+#include <cstddef>
+#include <vector>
+
+
+using namespace std;
+
+
+
+class Discard {
+public:
+    vector <Card> discard;
+    
+
+    void get_card(std::vector<sf::RectangleShape> &shape,DeckCard &row, int card_index);
+    void clear();
+        
+
+    
+    
+    
+};
+
+
+
+
+
+class MainDeck {
+    vector <Card> Deck;
+public:
+    MainDeck(int n, char ch){
+        if (ch=='m'){
+            for (int i=0;i<n;i++){
+            if (i%2==0){
+                Card card1(154,1);
+                Deck.push_back(card1);
+            }
+            else{
+                Card card2(151,1);
+                Deck.push_back(card2);
+            }   
+        }
+        }
+        else{
+        
+        for (int i=0;i<n;i++){
+            if (i%2==0){
+                Card card1(111,1);
+                Deck.push_back(card1);
+            }
+            else{
+                Card card2(142,1);
+                Deck.push_back(card2);
+            }   
+        }
+        }
+    }
+    
+
+    void shuffle_deck();
+    void append(Card one_card);
+    Card operator() (size_t pos);
+    size_t getSize();
+    void giveHand(DeckCard &hand,Discard &d,int n);
+    void pop_back();
+    Card del_back();
+    
+};
+
+
+
+
+
+#endif  // PROJECT_CLASSMAINDECK_UTILS_HPP_
---
+++ project/src/ClassMainDeck.cpp
@@ -0,0 +1,60 @@
+#include "../data/ClassMainDeck.hpp"
+#include <time.h>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void MainDeck::shuffle_deck() {
+    srand(time(NULL));
+    random_shuffle(Deck.begin(),Deck.end());
+
+}
+void MainDeck::append(Card one_card) {
+    Deck.push_back(one_card);
+}
+
+size_t MainDeck::getSize() {
+    return Deck.size();
+}
+
+Card MainDeck::operator() (size_t pos) {
+    return Deck.at(pos);
+}
+
+void MainDeck::giveHand(DeckCard &hand, Discard &d,int n){
+    for (int i=0;i<n;i++){
+        hand.deck_vec[i]=Deck[Deck.size()-1];
+        hand.avaliable[i]=0;
+        hand.deck_vec[i].TargetCard=4;
+        Deck.pop_back();
+        if (Deck.size()==0){
+            Deck.insert(Deck.end(), d.discard.begin(), d.discard.end());
+            d.clear();
+        }
+        
+
+    }
+}
+Card MainDeck::del_back(){
+    Card card(111,0);
+    card=Deck[Deck.size()-1];
+    Deck.pop_back();
+    return card;
+
+}
+void Discard::get_card(std::vector<sf::RectangleShape> &shape,DeckCard &row, int card_index){
+    discard.push_back(row.deck_vec[card_index]);
+    row.avaliable[card_index]=1;
+    Card empty_card(0,0);
+    shape[card_index].setTexture(empty_card.GetTexture());
+    row.deck_vec[card_index]=empty_card;
+}
+void Discard::clear(){
+    discard.clear();
+}
+
+
+
+
+
